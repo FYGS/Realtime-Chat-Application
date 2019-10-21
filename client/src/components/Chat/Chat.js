e59823834659dff383ae8eae3005d64135bb3251
Fernand SOUALO <fernandsoualo@gmail.com>
2019-10-21 22:25:59 +0100

    deployment OK

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -19,7 +19,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://realtime-chat-app-react.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
